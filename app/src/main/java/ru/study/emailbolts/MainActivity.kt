31169f911683e5d7ce354048c499c9e74f842d18
aspid168 <nikaleks168@gmail.com>
2021-07-12 15:05:00 +0300

    initial commit

---
+++ app/src/main/java/ru/study/emailbolts/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.study.emailbolts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
